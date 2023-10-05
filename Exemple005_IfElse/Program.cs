bbafb16cb520ec8bad504fb47d056920c270fa7c
Daria <dsobchik@gmail.com>
2023-10-05 13:38:06 +0300

    Example006_Base

---
+++ Exemple005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿global::System.Object value = Console.Write("Введите имя пользователя: ");
+string username = Consol.ReadLine();
+
+if(username == "Маша")
+{  
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
